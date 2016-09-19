2d5c2ed9ea7b7841c4ac4862e19b57d06f6edc9f
Kristina Urbas <k.urbas@semrush.com>
2016-09-19 19:06:29 +0300

    sync

---
+++ api-sync/script.js
@@ -0,0 +1,10 @@
+var _url = "http://us.backend.semrush.com/?jsoncallback=jQuery21407175669085129808_1474278537228&key=e33ea856d6dc4e9bfb227f8cea3a46d9&action=report&domain=ebay.com&type=domain_rank&display_hash=0410aa57863158cf6a231350e5a6ca62";
+
+var Library = Backbone.Collection.extend({
+	url: _url
+});
+
+var attrs = {
+	title: 'The Tempest',
+	author: 'Bill Shakespeare'
+}
