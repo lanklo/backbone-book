0f240a0aed5568a14c35b18c1d2eb605601bc9f4
Kristina Urbas <k.urbas@semrush.com>
2016-10-26 00:51:44 +0300

    add put, delete

--- example2/server.js
+++ example2/server.js
@@ -50,11 +50,16 @@ port, app.settings.env );
 	// подключение к бд
 	mongoose.connect('mongodb://localhost/library_database');
 
+	var Keywords = new mongoose.Schema({
+		Keyword: String
+	});
 	var Book = new mongoose.Schema({
 		title: String,
 		author: String,
-		releaseDate: Date
+		releaseDate: Date,
+		keywords: [ Keywords ]
 	});
+	
 	var BookModel = mongoose.model('Book', Book);
 
 	// маршруты
@@ -74,7 +79,8 @@ port, app.settings.env );
 		var book = new BookModel({
 			title: request.body.title,
 			author: request.body.author,
-			releaseDate: request.body.releaseDate
+			releaseDate: request.body.releaseDate,
+			keywords: request.body.keywords
 		});
 
 		book.save(function(err) {
@@ -88,7 +94,7 @@ port, app.settings.env );
 		});
 	});
 	//получение одной книги по id
-	app.get( '/api/books/:id', function( request, response ) {
+	app.get('/api/books/:id', function( request, response ) {
 		return BookModel.findById( request.params.id, function( err, book ) {
 			if( !err ) {
 				return response.send( book );
@@ -98,4 +104,38 @@ port, app.settings.env );
 		});
 	});
 
+	// обновление книги
+	api.get('/api/books/:id', function(request, response) {
+		return BookModel.findById(request.params.id, function(err, book) {
+			book.title = request.body.title;
+			book.author = request.body.author;
+			book.releaseDate = request.body.releaseDate;
+			book.keywords = request.body.keywords;
+			return book.save(function(err) {
+				if (!err) {
+					console.log('book updated');
+				} else {
+					console.log(err);
+				}
+
+				return response.send(book);
+			});
+		});
+	});
+
+	//удаление книги
+	app.delete( '/api/books/:id', function( request, response ) {
+		console.log( 'Deleting book with id: ' + request.params.id );
+		return BookModel.findById( request.params.id, function( err, book ) {
+			return book.remove( function( err ) {
+				if( !err ) {
+					console.log( 'Book removed' );
+					return response.send( '' );
+				} else {
+					console.log( err );
+				}
+			});
+		});
+	});
 });
+
